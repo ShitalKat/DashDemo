14d97d2066bd2ec528ac953a946ebc23548a88d3
ShitalK <shital.katkar@virtuaresearch.com>
2023-07-14 23:11:48 +0530

    Basic Page + Html css

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# GroceryStoreDashDemo
+# DashDemo Create Env
+python3 -m venv venv
+source venv/bin/activate
+
+# Installation
+https://dash.plotly.com/installation
+pip install dash
+pip install pandas
+
+# Create First App
+https://dash.plotly.com/minimal-app
---
+++ app.py
@@ -0,0 +1,39 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input #DCC stands for Dash Core Components
+import pandas as pd
+import plotly.express as px
+
+
+
+# Initialize the app
+app = Dash(__name__)
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=3),
+    # We can provide  static/dynamic graph in figure
+    dcc.Graph(figure={}, id='controls-and-graph')
+    #dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id= 'controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen): 
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+
